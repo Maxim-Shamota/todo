f8df97ccbb7346dfebcc27d970c5e8819fd4097e
Maxim <xbultazar@gmail.com>
2022-06-17 12:39:51 +0300

    add view categories at sidebar

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Category} from "../../model/Category";
 
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+	selector: 'app-categories',
+	templateUrl: './categories.component.html',
+	styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+	categories: Category[] | undefined;
 
-  ngOnInit(): void {
-  }
+	constructor(private dataHandler: DataHandlerService) {
+	}
+
+	// метод вызывается автоматически после инициализации компонента
+	ngOnInit(): void {
+		this.categories = this.dataHandler.getCategories();
+	}
 
 }
